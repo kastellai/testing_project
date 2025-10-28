87f1e3c400ae3967377681d399f9ee96642236e2
kastellai <ilenia.castellana@gmail.com>
2025-10-28 12:44:04 +0100

    login page

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import { useState } from 'react';
+
+function App() {
+
+  const [ userName, setUserName ] = useState("")
+  const handleClick = (newName) => {setUserName(newName);}
+
+  return (
+    
+     <Routes>
+        <Route path="/" element={<LoginPage />} />
+        { userName === "" 
+          ? <Route path="/home" element={<HomePage />} />
+          : <Route path="/login" element={<LoginPage onLoginEvent={handleClick}/>} />}
+
+             
+         {/* <Route path="/home" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage onLoginEvent={handleClick}/>} /> */}
+
+        
+      </Routes>
+
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import "../LoginPage/style.css"
+// TODO: come rendere il pulsante stessa larghezza
+
+
+const LoginPage = ({onLoginEvent}) => {
+    const [ usernameInput, setUsernameInput ] = useState("");
+    const [ passwordInput, setPasswordInput ] = useState("");
+    const [ userData, setUserData ] = useState({});
+
+    const handlerUsernameInput = (event) => {
+        setUsernameInput(event.target.value)
+    }
+
+    const handlerPasswordInput = (event) => {
+        setPasswordInput(event.target.value)
+    }
+
+    useEffect(() => {
+        setUserData({username: usernameInput, password: passwordInput});
+    }, [usernameInput, passwordInput])
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUserData({
+            name: usernameInput,
+            password: passwordInput
+        });
+        console.log("USER DATA - NAME: " + userData.name)
+        console.log("USER DATA - PASSWORD: " + userData.name)
+        onLoginEvent(userData.name);
+    }
+
+    return (
+        <form className="loginForm">
+            <p>Sign In on XxX</p>
+            <input value={usernameInput} onChange={handlerUsernameInput} type="text" placeholder="Username" required autoComplete="false"></input>
+            <input value={passwordInput} onChange={handlerPasswordInput} type="password" placeholder="Password" required autoComplete="false"></input>
+            <input value="LOGIN" onClick={handleSubmit} type="submit" className="loginFormButton"></input>
+        </form>
+    );
+}
+
+export default LoginPage;
